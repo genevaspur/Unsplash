76c14300a497cec0c20efe84024117214383f664
이학성 <kth4241@gmail.com>
2020-12-22 17:10:16 +0900

    update alert 확장함수로 변경

--- app/src/main/java/com/example/unsplash/activity/BindingActivity.kt
+++ app/src/main/java/com/example/unsplash/activity/BindingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.ViewDataBinding
 import com.example.unsplash.BR
 import com.example.unsplash.R
@@ -30,35 +31,27 @@ abstract class BindingActivity<VM: BaseViewModel, V: ViewDataBinding> : BaseActi
         showErrorAlert(throwable.toString())
     }
 
-    protected fun <T: Any> showUpdateAlert(force: Boolean, targetActivity: Class<T>) {
+    protected fun <T: BaseActivity<*>> showUpdateAlert(force: Boolean, targetActivity: Class<T>) {
 
-        val message: String
-        val cancelable: Boolean
+        val message: String = if (force) getString(R.string.msg_force_update)
+        else getString(R.string.msg_update)
 
-        if (force) {
-            message = getString(R.string.msg_force_update)
-            cancelable = false
-        } else {
-            message = getString(R.string.msg_update)
-            cancelable = true
-        }
-
-        val updateDialog = AlertDialog.Builder(this)
-            .setMessage(message)
-            .setCancelable(cancelable)
-            .setPositiveButton(R.string.positive_btn) { _, _ ->
-                vm.updateApplication(force)
-            }
-
-        if (cancelable) updateDialog.setNegativeButton(R.string.negative_btn) { _, _ ->
-            changeActivity(targetActivity)
-        }
-
-        updateDialog.create().show()
+        AlertDialog.Builder(this)
+                .setMessage(message)
+                .setCancelable(!force)
+                .setPositiveButton(R.string.positive_btn) { _, _ -> vm.updateApplication(force) }
+                .isCancelable(force) { changeActivity(targetActivity) }
+                .create()
+                .show()
 
     }
 
-    protected fun <T: Any> changeActivity(activity: Class<T>) {
+    private fun AlertDialog.Builder.isCancelable(force: Boolean, block: () -> Unit): AlertDialog.Builder {
+        if (!force) this.setNegativeButton(R.string.negative_btn) { _, _ -> block() }
+        return this
+    }
+
+    protected fun <T: BaseActivity<*>> changeActivity(activity: Class<T>) {
         startActivity(Intent(this, activity))
         finish()
     }
